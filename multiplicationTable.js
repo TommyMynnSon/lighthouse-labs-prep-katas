da991cf52c7b6aec99da7a3d6482487e2a94920b
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-17 02:14:11 -0700

    Implement and test multiplicationTable function

---
+++ multiplicationTable.js
@@ -0,0 +1,23 @@
+const multiplicationTable = function(maxValue) {
+  // Your code here
+  let result = '';
+
+  for (let i = 1; i <= maxValue; i++) {
+    for (let j = 1; j <= maxValue; j++) {
+      result += (i * j);
+
+      if (j !== maxValue) {
+        result += ' ';
+      }
+    }
+
+ 
+    result += '\n';
+  }
+
+  return result;
+};
+
+console.log(multiplicationTable(1));
+console.log(multiplicationTable(5));
+console.log(multiplicationTable(10));
